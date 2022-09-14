8a6cf11a50f903aa656a5339fe64c7c2b81a6116
Konrad Dawydiuk <kd250241@ncr.com>
2022-09-14 08:41:50 +0200

    Initial commit

---
+++ src/main/java/pl/home/helper/HhHomeHelperShoppingApplication.java
@@ -0,0 +1,13 @@
+package pl.home.helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhHomeHelperShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhHomeHelperShoppingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/home/helper/HhHomeHelperShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.home.helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhHomeHelperShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
